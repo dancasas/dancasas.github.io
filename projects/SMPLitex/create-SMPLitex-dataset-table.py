78c57c5699fbe9c817fd0be26d0928bd6c51f62b
Dan Casas <dan.casas@gmail.com>
2023-12-16 12:18:53 +0100

    Dataset moved

---
+++ projects/SMPLitex/create-SMPLitex-dataset-table.py
@@ -0,0 +1,27 @@
+base_url = "./SMPLitex-dataset/textures/"
+base_gif_url = "./SMPLitex-dataset/360-gifs/"
+
+start_image = 0
+end_image = 100  # Replace with the desired end image number
+image_prefix = "SMPLitex-texture-"
+
+# Creating the HTML code
+html_code = ""
+
+for image_number in range(start_image, end_image, 2):
+    html_code += '<tr>\n'
+    html_code += f'  <td colspan="2" align="center"><a href="{base_url}{image_prefix}{str(image_number).zfill(5)}.png">{image_prefix}{str(image_number).zfill(5)}</a></td>\n'
+    html_code += f'  <td colspan="2" align="center"><a href="{base_url}{image_prefix}{str(image_number + 1).zfill(5)}.png">{image_prefix}{str(image_number + 1).zfill(5)}</a></td>\n'
+    html_code += '</tr>\n'
+    html_code += '<tr>\n'
+    html_code += f'  <td> <img src="{base_url}{image_prefix}{str(image_number).zfill(5)}.png"  alt="1" width = 150px></td>\n'
+    html_code += f'  <td><img src="{base_gif_url}{image_prefix}{str(image_number).zfill(5)}.gif" alt="2" width = 150px></td>\n'
+    html_code += f'  <td> <img src="{base_url}{image_prefix}{str(image_number + 1).zfill(5)}.png"  alt="1" width = 150px></td>\n'
+    html_code += f'  <td><img src="{base_gif_url}{image_prefix}{str(image_number + 1).zfill(5)}.gif" alt="2" width = 150px></td>\n'
+    html_code += '</tr>'
+
+# Writing the HTML code to a file
+with open("SMPLitex-dataset-table.html", "w") as file:
+    file.write(html_code)
+
+print("HTML code written to SMPLitex-dataset-table.html")
